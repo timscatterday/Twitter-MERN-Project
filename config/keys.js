b0b853a7cd1d8a7c0951dfcb2f0fa445cb6ac3e8
Tim Scatterday <timothyscatterday@outlook.com>
2019-11-17 14:04:02 -0800

    First commit Twitter MERN Project

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://timscatterday:TYBdG9XPnPQksPPJ@cluster0-ymki1.mongodb.net/test?retryWrites=true&w=majority'
+    //Make sure this is your own unique string
+}
